ad803d30563d065188889c31347726b899e6b494
Victor Milovanov <lost@losttech.software>
2020-09-05 16:31:02 -0700

    added some model documentation

--- src/Siren.cs
+++ src/Siren.cs
@@ -7,14 +7,59 @@
 
     using tensorflow.keras.layers;
 
+    /// <summary>
+    /// Implements <a href="https://vsitzmann.github.io/siren/">SIREN: Implicit Neural Representations with Periodic Activation Functions</a>
+    /// - a network with sine activations.
+    ///
+    /// <para>All SIREN inputs must be normalized to [-1;1] interval.</para>
+    /// </summary>
     public class Siren : Model {
         readonly Dense[] innerLayers;
+        /// <summary>
+        /// Frequency scale for input layer (<c>first_omega_0</c> in the paper).
+        /// <para>The layer formula is <c>sin(FreqScale*(Weight*input+Bias)</c></para>
+        /// </summary>
         public float InnerFrequencyScale { get; }
+        /// <summary>
+        /// Frequency scale for intermediate layers (<c>omega_0</c> in the paper).
+        /// <para>The intermediate layer formula is <c>sin(FreqScale*(Weight*prevOutput+Bias)</c></para>
+        /// </summary>
         public float InputFrequencyScale { get; }
 
+        /// <summary>Frequency scales, recommended in the paper ("omega_0")</summary>
+        public static class RecommendedFrequencyScales {
+            internal const float Default = 30;
+
+            /// <summary>Frequency scale for inner layers, any data type</summary>
+            public static readonly float Inner = 30;
+            /// <summary>Frequency scale for input layer for images</summary>
+            public static readonly float ImageInput = 30;
+            /// <summary>Frequency scale for input layer for sound (raw wav)</summary>
+            public static readonly float SoundInput = 3000;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Siren"/> of the specified size.
+        /// </summary>
+        /// <param name="inputSize">
+        /// Number of inputs.
+        /// This MUST be set to the correct value, otherwise the network might not be trainable.
+        /// </param>
+        /// <param name="innerSizes">
+        /// Sizes of inner layers.
+        /// Must be a non-empty array of positive numbers.</param>
+        /// <param name="inputFrequencyScale">
+        /// Frequency scale for input layer (<c>first_omega_0</c> in the paper).
+        /// <para>The layer formula is <c>sin(FreqScale*(Weight*input+Bias)</c></para>
+        /// <para>See <see cref="RecommendedFrequencyScales"/> for good values.</para>
+        /// </param>
+        /// <param name="innerFrequencyScale">
+        /// Frequency scale for intermediate layers (<c>omega_0</c> in the paper).
+        /// <para>The intermediate layer formula is <c>sin(FreqScale*(Weight*prevOutput+Bias)</c></para>
+        /// </param>
         public Siren(int inputSize, int[] innerSizes,
-                     float inputFrequencyScale = 30.0f,
-                     float innerFrequencyScale = 30.0f) {
+                     float inputFrequencyScale = RecommendedFrequencyScales.Default,
+                     float innerFrequencyScale = RecommendedFrequencyScales.Default) {
             if (inputSize <= 0)
                 throw new ArgumentOutOfRangeException(nameof(inputSize));
             if (innerSizes is null || innerSizes.Length == 0)
@@ -33,6 +78,9 @@
 
             int currentInputSize = inputSize;
             for (int innerIndex = 0; innerIndex < innerSizes.Length; innerIndex++) {
+                // This is the crucial part of the paper.
+                // Without proper weight initialization training will fail.
+                // See paper sec. 3.2
                 double weightLimits = innerIndex > 0
                     ? Math.Sqrt(6.0f / currentInputSize) / this.InnerFrequencyScale
                     : 1.0f / inputSize;
