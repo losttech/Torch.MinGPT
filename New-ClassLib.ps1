283f66dac27c3c444337bb87d04eee1d2a7085d1
Victor Milovanov <lost@losttech.software>
2023-01-15 10:46:31 -0800

    added New-ClassLib script

---
+++ New-ClassLib.ps1
@@ -0,0 +1,20 @@
+param ([Parameter(Mandatory=$true)] $Name)
+
+git clone https://github.com/losttech/Templates.ClassLib.git "$Name"
+cd "$Name"
+git remote rename origin template
+git branch --unset-upstream
+Move-Item Templates.ClassLib.sln "$Name.sln"
+Move-Item src/ClassLib.csproj "src/$Name.csproj"
+Move-Item test/ClassLib.Tests.csproj "test/$Name.Tests.csproj"
+
+function Replace-InFile($path, $what, $with){
+    $content = Get-Content "$path" -Raw
+    $updated = $content -replace $what,$with
+    Set-Content -Path "$path" $updated
+}
+
+Replace-InFile "$Name.sln" "ClassLib" "$Name"
+Replace-InFile "test/$Name.Tests.csproj" "ClassLib" "$Name"
+
+Remove-Item New-ClassLib.ps1
