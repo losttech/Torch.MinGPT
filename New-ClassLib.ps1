c92c07b54f5c7934bae89b12ca43962bea2f1125
Victor Nova <lost@losttech.software>
2023-04-15 13:15:14 -0700

    added README to the project template

--- New-ClassLib.ps1
+++ New-ClassLib.ps1
@@ -4,9 +4,13 @@ git clone https://github.com/losttech/Templates.ClassLib.git "$Name"
 cd "$Name"
 git remote rename origin template
 git branch --unset-upstream
+
 Move-Item Templates.ClassLib.sln "$Name.sln"
 Move-Item src/ClassLib.csproj "src/$Name.csproj"
 Move-Item test/ClassLib.Tests.csproj "test/$Name.Tests.csproj"
+
+Remove-Item README.md
+Move-Item README.template.md README.md
 
 function Replace-InFile($path, $what, $with){
     $content = Get-Content "$path" -Raw
@@ -16,5 +20,7 @@ function Replace-InFile($path, $what, $with){
 
 Replace-InFile "$Name.sln" "ClassLib" "$Name"
 Replace-InFile "test/$Name.Tests.csproj" "ClassLib" "$Name"
+Replace-InFile "README.md" "ClassLib" "$Name"
+
 
 Remove-Item New-ClassLib.ps1
---
+++ README.template.md
@@ -0,0 +1,1 @@
+[![NuGet](https://img.shields.io/nuget/v/ClassLib.svg)](https://www.nuget.org/packages/ClassLib/)
